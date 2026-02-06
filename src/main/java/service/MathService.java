698ad837c7512eb3a2b920be3570e7b1fd1d5642
k0alita <alejandrohernandossan@gmail.com>
2026-02-06 14:31:35 +0100

    Metodo suma y test unitario

--- src/main/java/service/MathService.java
+++ src/main/java/service/MathService.java
@@ -1,7 +1,7 @@
 package service;
 
 public class MathService {
-    public double suma(double num1, double num2) {
-        return num1 + num2;
+    public int sumar(int a, int b) {
+        return a + b;
     }
 }
